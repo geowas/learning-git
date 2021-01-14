ea04345683bc827b2559a950f69a03157a9a77e3
Marcin Wąsowski <mar.wasowski@gmail.com>
2021-01-14 21:21:07 +0100

    Add sum

--- task.py
+++ task.py
@@ -2,4 +2,11 @@ print('Lista zakupów')
 print()
 slownik = {'piekarnia': ['chleb', 'pączek', 'bułki'], 'warzywniak': ['marchew', 'seler', 'rukola']}
 
+suma=0
+for klucz in slownik:
+    suma+=len(slownik[klucz])
+    #print('suma: {}'.format(suma))
+    print('Idę do {}, kupuję tu następujące rzeczy: {}'.format(klucz.capitalize(), [x.capitalize() for x in slownik[klucz]] ))
+   
+    #print(idę do klucz i kupuję tam)
 
